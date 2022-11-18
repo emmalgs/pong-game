3fc33931eae65eaf4d9e84b0b2b83f876615df93
Emma Gerigscott <elgerig@gmail.com>
2022-11-18 10:48:15 -0800

    Set up js files, html, styles

---
+++ Ball.js
@@ -0,0 +1,35 @@
+export default class Ball {
+    constructor(ballElem) {
+        this.ballElem = ballElem
+    }
+
+    get x() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue('--x'))
+    }
+
+    set x(value) {
+        this.ballElem.style.setProperty('--x', value)
+    }
+
+    get y() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue('--y'))
+    }
+
+    set y(value) {
+        this.ballElem.style.setProperty('--y', value)
+    }
+
+    reset() {
+        this.x = 50
+        this.y = 50
+        this.direction = { x: 0.75, y: 0.5 }
+        while (true) {
+            const heading = randomNumberBetween(0, 2 * Math.PI)
+            this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
+        }
+    }
+
+    update(delta) {
+
+    }
+}
